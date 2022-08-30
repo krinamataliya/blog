a1f5c9db97363660898e313aa114028affb27c62
Krina Mataliya <krina.piyushbhaimataliya@freshworks.com>
2022-08-30 16:04:26 +0530

    Added comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+    #These validations will be called if the object is saved into db
+   validates:title, presence:true  
+   validates:content,presence:true
+    
 end
